23d1cb33303cc8015cfc5029d6177d21429f0b26
Joshua <jdkelly2015@gmail.com>
2019-12-05 20:40:55 -0500

    flipped a few more things around

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.css";
-import LandingPage from "./components/LandingPage";
+import NavBar from "./components/NavBar";
+import Bio from "./components/Bio";
+import MyPortfolio from "./components/MyPortfolio";
+import ContactMe from "./components/ContactMe";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+      <NavBar />
+      <Home />
+      <Bio />
+      <MyPortfolio />
+      <ContactMe />
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 const Home = props => (
   <section className="container-fluid text-white bg-dark" id="home">
     <Container>
@@ -12,20 +13,20 @@ const Home = props => (
         </Col>
       </Row>
       <Row className="justify-content-around">
-        <a
-          className="btn btn-primary btn-lg mr-2"
+        <Button
+          size="lg"
+          className="mr-2"
           href="https://github.com/Josh-Kelly2015"
-          role="button"
         >
           Github
-        </a>
-        <a
-          className="btn btn-primary btn-lg ml-2"
+        </Button>
+        <Button
+          className="ml-2"
           href="https://www.linkedin.com/in/joshua-kelly-482196185/"
-          role="button"
+          size="lg"
         >
           LinkedIn
-        </a>
+        </Button>
       </Row>
     </Container>
   </section>
