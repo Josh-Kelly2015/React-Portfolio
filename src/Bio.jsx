0698368fffa7ea655a7127bf627b487539933f63
Joshua <jdkelly2015@gmail.com>
2019-12-07 15:35:36 -0500

    going over all pages. making them 100%  react-bootstrap

--- src/Bio.jsx
+++ src/Bio.jsx
@@ -4,64 +4,63 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
+import Jumbotron from "react-bootstrap/Jumbotron";
 import ComputerBackground from "./Images/technics-q-c-640-480-1.jpg";
 const AboutMe = props => (
-  <section className="container-fluid bg-light" id="bio">
-    <Container className="mt-5">
-      <Row className="justify-content-center">
-        <h1>About Me</h1>
+  <Container fluid className=" bg-light">
+    <Row className="justify-content-center">
+      <h1>About Me</h1>
+    </Row>
+    <Jumbotron>
+      <Row>
+        <Col lg="4">
+          <img
+            className="w-100 rounded-circle"
+            src={ComputerBackground}
+            alt="Selfieeee"
+          />
+        </Col>
+        <Col lg="8">
+          <h5>
+            Natural logical thinker, inspired to pursue a greater cause.
+            Currently working as a full-time employee for Telecom Innovations,
+            as a low-voltage-technician. Experienced in HTML5, CSS3, Javascript,
+            JQuery, Bootstrap, ReactJs, Node Js, MySQL, MongoDB, Express and
+            Handlebars JS. Superior analyst and critical thinker searching for a
+            position in frontend development.
+          </h5>
+        </Col>
       </Row>
-      <div className="jumbotron bg-transparent shadow-lg p-4">
-        <Row>
-          <Col lg="4">
-            <img
-              className="w-100 rounded-circle"
-              src={ComputerBackground}
-              alt="Selfieeee"
-            />
-          </Col>
-          <Col lg="8" className="mt-5">
-            <h5 className="mt-4">
-              Natural logical thinker, inspired to pursue a greater cause.
-              Currently working as a full-time employee for Telecom Innovations,
-              as a low-voltage-technician. Experienced in HTML5, CSS3,
-              Javascript, JQuery, Bootstrap, ReactJs, Node Js, MySQL, MongoDB,
-              Express and Handlebars JS. Superior analyst and critical thinker
-              searching for a position in frontend development.
-            </h5>
-          </Col>
-        </Row>
 
-        <Row className="mt-4">
-          <Col>
-            <h3>Skills</h3>
-            <ListGroup>
-              <Card.Header>
-                React.JS, Handlebars, BootStrap, Flex-Box, Media Queries
-              </Card.Header>
-              <Card.Header> HTML5, JavaScript, CSS3, jQuery</Card.Header>
-              <Card.Header> MongoDB, MySQL, Mongoose, Sequelize</Card.Header>
-              <Card.Header>Node Js, Express, Axios</Card.Header>
-              <Card.Header>RESTful API's, AJAX, JSON, GIT</Card.Header>
-              <Card.Header>
-                MERN Stack, Postman, Robo3T, Adobe XD, GitHub, Heroku
-              </Card.Header>
-            </ListGroup>
-          </Col>
+      <Row>
+        <Col>
+          <h3>Skills</h3>
+          <ListGroup>
+            <Card.Header>
+              React.JS, Handlebars, BootStrap, Flex-Box, Media Queries
+            </Card.Header>
+            <Card.Header> HTML5, JavaScript, CSS3, jQuery</Card.Header>
+            <Card.Header> MongoDB, MySQL, Mongoose, Sequelize</Card.Header>
+            <Card.Header>Node Js, Express, Axios</Card.Header>
+            <Card.Header>RESTful API's, AJAX, JSON, GIT</Card.Header>
+            <Card.Header>
+              MERN Stack, Postman, Robo3T, Adobe XD, GitHub, Heroku
+            </Card.Header>
+          </ListGroup>
+        </Col>
 
-          <Col>
-            <h3>Goals</h3>
-            <ListGroup>
-              <Card.Header> Experiment with CSS animations</Card.Header>
-              <Card.Header>Develop deeper JavaScript skills</Card.Header>
-              <Card.Header>Always learn, Always Grow</Card.Header>
-              <Card.Header>Attend Local Networking Events</Card.Header>
-              <Card.Header>Study Angular, Vue, Python, C#</Card.Header>
-            </ListGroup>
-          </Col>
-        </Row>
-      </div>
-    </Container>
-  </section>
+        <Col>
+          <h3>Goals</h3>
+          <ListGroup>
+            <Card.Header> Experiment with CSS animations</Card.Header>
+            <Card.Header>Develop deeper JavaScript skills</Card.Header>
+            <Card.Header>Always learn, Always Grow</Card.Header>
+            <Card.Header>Attend Local Networking Events</Card.Header>
+            <Card.Header>Study Angular, Vue, Python, C#</Card.Header>
+          </ListGroup>
+        </Col>
+      </Row>
+    </Jumbotron>
+  </Container>
 );
 export default AboutMe;
