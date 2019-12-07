dcb7c59d0837a9389a62cb9ab1cd93ff3741ce23
Joshua <jdkelly2015@gmail.com>
2019-12-07 16:40:04 -0500

    added one more container to home

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -4,23 +4,25 @@ import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 const Home = props => (
   <Container fluid>
-    <Row className="justify-content-center">
-      <h1>Joshua Kelly</h1>
-    </Row>
-    <Row className="justify-content-center">
-      <p>Full Stack Web Developer</p>
-    </Row>
-    <Row className="justify-content-around">
-      <Button size="lg" href="https://github.com/Josh-Kelly2015">
-        Github
-      </Button>
-      <Button
-        size="lg"
-        href="https://www.linkedin.com/in/joshua-kelly-482196185/"
-      >
-        LinkedIn
-      </Button>
-    </Row>
+    <Container>
+      <Row className="justify-content-center">
+        <h1>Joshua Kelly</h1>
+      </Row>
+      <Row className="justify-content-center">
+        <p>Full Stack Web Developer</p>
+      </Row>
+      <Row className="justify-content-around">
+        <Button size="lg" href="https://github.com/Josh-Kelly2015">
+          Github
+        </Button>
+        <Button
+          size="lg"
+          href="https://www.linkedin.com/in/joshua-kelly-482196185/"
+        >
+          LinkedIn
+        </Button>
+      </Row>
+    </Container>
   </Container>
 );
 export default Home;
