30b4340f1e4150bf8ebc7aff23342b6e66fadf27
Joshua <jdkelly2015@gmail.com>
2019-12-07 16:36:33 -0500

    changed my mind and wanted a jumbotron around the carsouel

--- src/MyPortfolio.jsx
+++ src/MyPortfolio.jsx
@@ -4,71 +4,75 @@ import Carousel from "react-bootstrap/Carousel";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
-
+import Jumbotron from "react-bootstrap/Jumbotron";
 const MyPortfolio = props => (
   <Container fluid>
     <Row className="justify-content-center">
       <h1>My Portfolio</h1>
     </Row>
     <Container>
-      <Carousel className="shadow-lg">
-        <Carousel.Item>
-          <img
-            src={ComputerBackground}
-            className="d-block w-100"
-            alt="RPG Game"
-          />
-          <Carousel.Caption>
-            <h1>Evolve Business</h1>
-            <p>Helping businesses over come innefficincies</p>
-            <Button
-              variant="primary"
-              href="https://finalprojectthree.herokuapp.com/"
-              size="lg"
-            >
-              Heroku
-            </Button>
-          </Carousel.Caption>
-        </Carousel.Item>
+      <Jumbotron className="shadow-lg">
+        <Carousel>
+          <Carousel.Item>
+            <img
+              src={ComputerBackground}
+              className="d-block w-100"
+              alt="RPG Game"
+            />
+            <Carousel.Caption>
+              <h1>Evolve Business</h1>
+              <p>Helping businesses over come innefficincies</p>
+              <Button
+                variant="primary"
+                href="https://finalprojectthree.herokuapp.com/"
+                size="lg"
+              >
+                Heroku
+              </Button>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-        <Carousel.Item>
-          <img
-            src={ComputerBackground}
-            className="d-block w-100"
-            alt="RPG Game"
-          />
-          <Carousel.Caption>
-            <h1>Car Med</h1>
-            <p>A car maintenance and overall car health management dashboard</p>
-            <Button
-              variant="primary"
-              href="https://afternoon-wave-65413.herokuapp.com/"
-              size="lg"
-            >
-              Heroku
-            </Button>
-          </Carousel.Caption>
-        </Carousel.Item>
+          <Carousel.Item>
+            <img
+              src={ComputerBackground}
+              className="d-block w-100"
+              alt="RPG Game"
+            />
+            <Carousel.Caption>
+              <h1>Car Med</h1>
+              <p>
+                A car maintenance and overall car health management dashboard
+              </p>
+              <Button
+                variant="primary"
+                href="https://afternoon-wave-65413.herokuapp.com/"
+                size="lg"
+              >
+                Heroku
+              </Button>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-        <Carousel.Item>
-          <img
-            src={ComputerBackground}
-            className="d-block w-100"
-            alt="RPG Game"
-          />
-          <Carousel.Caption>
-            <h1>T.O.A.D</h1>
-            <p>An app to help managers assign employees to projects</p>
-            <Button
-              variant="primary"
-              href="https://afternoon-wave-65413.herokuapp.com/"
-              size="lg"
-            >
-              Heroku
-            </Button>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+          <Carousel.Item>
+            <img
+              src={ComputerBackground}
+              className="d-block w-100"
+              alt="RPG Game"
+            />
+            <Carousel.Caption>
+              <h1>T.O.A.D</h1>
+              <p>An app to help managers assign employees to projects</p>
+              <Button
+                variant="primary"
+                href="https://afternoon-wave-65413.herokuapp.com/"
+                size="lg"
+              >
+                Heroku
+              </Button>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </Jumbotron>
     </Container>
   </Container>
 );
