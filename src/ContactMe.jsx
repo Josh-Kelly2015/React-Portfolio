7f08d50742d5b4f2cd430e8e5f8fa0b241c5aac9
Joshua <jdkelly2015@gmail.com>
2019-12-07 15:45:29 -0500

    fixed all the code

--- src/ContactMe.jsx
+++ src/ContactMe.jsx
@@ -2,74 +2,72 @@ import React from "react";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Button from "react-bootstrap/Button";
 const ContactMe = props => (
-  <section className=" container-fluid bg-light" id="contactMe">
-    <Container>
-      <Row className="justify-content-center">
-        <h1>Contact Me</h1>
-      </Row>
-      <div className="jumbotron bg-transparent shadow-lg p-4">
-        <form className="needs-validation " novalidate>
-          <Row>
-            <Col>
-              <label for="validationCustom01">First name</label>
-              <textarea
-                className="form-control"
-                id="validationTextarea"
-                placeholder="John"
-                required
-              ></textarea>
-              <div className="valid-feedback">Looks good!</div>
-            </Col>
-            <Col>
-              <label for="validationCustom02">Last name</label>
-              <textarea
-                className="form-control"
-                id="validationTextarea"
-                placeholder="Doe"
-                required
-              ></textarea>
-              <div className="valid-feedback">Looks good!</div>
-            </Col>
-          </Row>
+  <Container fluid className="bg-light">
+    <Row className="justify-content-center">
+      <h1>Contact Me</h1>
+    </Row>
+    <Jumbotron className="shadow-lg">
+      <form className="needs-validation " novalidate>
+        <Row>
+          <Col>
+            <label for="validationCustom01">First name</label>
+            <textarea
+              className="form-control"
+              id="validationTextarea"
+              placeholder="John"
+              required
+            ></textarea>
+            <div className="valid-feedback">Looks good!</div>
+          </Col>
+          <Col>
+            <label for="validationCustom02">Last name</label>
+            <textarea
+              className="form-control"
+              id="validationTextarea"
+              placeholder="Doe"
+              required
+            ></textarea>
+            <div className="valid-feedback">Looks good!</div>
+          </Col>
+        </Row>
 
-          <Row>
-            <Col>
-              <label for="validationCustom03">Email</label>
-              <textarea
-                className="form-control"
-                id="validationTextarea"
-                placeholder="example123@domain.com "
-                required
-              ></textarea>
-              <div className="invalid-feedback">
-                Please provide a valid Email Address
-              </div>
-            </Col>
-          </Row>
+        <Row>
+          <Col>
+            <label for="validationCustom03">Email</label>
+            <textarea
+              className="form-control"
+              id="validationTextarea"
+              placeholder="example123@domain.com "
+              required
+            ></textarea>
+            <div className="invalid-feedback">
+              Please provide a valid Email Address
+            </div>
+          </Col>
+        </Row>
 
-          <Row>
-            <Col>
-              <label for="validationTextarea">Message</label>
-              <textarea
-                className="form-control"
-                id="validationTextarea"
-                placeholder="Click here to begin typing... "
-                required
-              ></textarea>
-              <div className="invalid-feedback">
-                Please enter a message in the textarea.
-              </div>
-            </Col>
-          </Row>
-          <Row className="justify-content-center">
-            <button id="submitButton" className="btn btn-primary" type="submit">
-              Submit form
-            </button>
-          </Row>
-        </form>
-      </div>
-    </Container>
-  </section>
+        <Row>
+          <Col>
+            <label for="validationTextarea">Message</label>
+            <textarea
+              className="form-control"
+              id="validationTextarea"
+              placeholder="Click here to begin typing... "
+              required
+            ></textarea>
+            <div className="invalid-feedback">
+              Please enter a message in the textarea.
+            </div>
+          </Col>
+        </Row>
+        <Row className="justify-content-center mt-2">
+          <Button variant="primary">Submit form</Button>
+        </Row>
+      </form>
+    </Jumbotron>
+  </Container>
 );
 export default ContactMe;
--- src/MyPortfolio.jsx
+++ src/MyPortfolio.jsx
@@ -4,94 +4,72 @@ import Carousel from "react-bootstrap/Carousel";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
+
 const MyPortfolio = props => (
-  <section className="container-fluid text-white bg-dark" id="myPortfolio">
+  <Container fluid className="text-white bg-dark">
+    <Row className="justify-content-center">
+      <h1>My Portfolio</h1>
+    </Row>
     <Container>
-      <Row className="justify-content-center">
-        <h1>My Portfolio</h1>
-      </Row>
       <Carousel>
         <Carousel.Item>
-          <div class="cardContainer">
-            <div class="card">
-              <input type="checkbox" name="" />
-              <div className="toggle">+</div>
-              <img
-                src={ComputerBackground}
-                className="d-block w-100 imgBx"
-                alt="RPG Game"
-              />
-              <Carousel.Caption className="content">
-                <h1>Evolve Business</h1>
-                {/* <p>
-                  A car maintenance and overall car health management dashboard
-                </p> */}
-                <Button
-                  variant="primary"
-                  href="https://finalprojectthree.herokuapp.com/"
-                  size="lg"
-                >
-                  Heroku
-                </Button>
-              </Carousel.Caption>
-            </div>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div class="cardContainer">
-            <div class="card">
-              <input type="checkbox" name="" />
-              <div className="toggle">+</div>
-              <img
-                src={ComputerBackground}
-                className="d-block w-100 imgBx"
-                alt="RPG Game"
-              />
-              <Carousel.Caption className="content">
-                <h1>Car Med</h1>
-                {/* <p>
-                  A car maintenance and overall car health management dashboard
-                </p> */}
-                <Button
-                  variant="primary"
-                  href="https://afternoon-wave-65413.herokuapp.com/"
-                  size="lg"
-                >
-                  Heroku
-                </Button>
-              </Carousel.Caption>
-            </div>
-          </div>
+          <img
+            src={ComputerBackground}
+            className="d-block w-100 imgBx"
+            alt="RPG Game"
+          />
+          <Carousel.Caption className="content">
+            <h1>Evolve Business</h1>
+            <p>Helping businesses over come innefficincies</p>
+            <Button
+              variant="primary"
+              href="https://finalprojectthree.herokuapp.com/"
+              size="lg"
+            >
+              Heroku
+            </Button>
+          </Carousel.Caption>
         </Carousel.Item>
 
         <Carousel.Item>
-          <div class="cardContainer">
-            <div class="card">
-              <input type="checkbox" name="" />
-              <div className="toggle">+</div>
-              <img
-                src={ComputerBackground}
-                className="d-block w-100 imgBx"
-                alt="RPG Game"
-              />
-              <Carousel.Caption className="content">
-                <h1>T.O.A.D</h1>
-                {/* <p>
-                  A car maintenance and overall car health management dashboard
-                </p> */}
-                <Button
-                  variant="primary"
-                  href="https://afternoon-wave-65413.herokuapp.com/"
-                  size="lg"
-                >
-                  Heroku
-                </Button>
-              </Carousel.Caption>
-            </div>
-          </div>
+          <img
+            src={ComputerBackground}
+            className="d-block w-100 imgBx"
+            alt="RPG Game"
+          />
+          <Carousel.Caption>
+            <h1>Car Med</h1>
+            <p>A car maintenance and overall car health management dashboard</p>
+            <Button
+              variant="primary"
+              href="https://afternoon-wave-65413.herokuapp.com/"
+              size="lg"
+            >
+              Heroku
+            </Button>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            src={ComputerBackground}
+            className="d-block w-100 imgBx"
+            alt="RPG Game"
+          />
+          <Carousel.Caption>
+            <h1>T.O.A.D</h1>
+            <p>An app to help managers assign employees to projects</p>
+            <Button
+              variant="primary"
+              href="https://afternoon-wave-65413.herokuapp.com/"
+              size="lg"
+            >
+              Heroku
+            </Button>
+          </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
     </Container>
-  </section>
+  </Container>
 );
 export default MyPortfolio;
